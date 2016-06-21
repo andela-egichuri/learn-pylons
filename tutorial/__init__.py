ddef07aa7ade0cbcf7cfa6595ad4d521c892b28b
Eric Gichuri <eric.gichuri@andela.com>
2016-06-21 19:32:45 +0300

    Forms: Deform and validation:Colander

--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -3,12 +3,12 @@ from pyramid.session import SignedCookieSessionFactory
 
 
 def main(global_config, **settings):
-    my_session_factory = SignedCookieSessionFactory(
-        'itsaseekreet')
-    config = Configurator(settings=settings,
-                          session_factory=my_session_factory)
+    config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
-    config.add_route('home', '/')
-    config.add_route('hello', '/howdy')
+    config.add_route('wiki_view', '/')
+    config.add_route('wikipage_add', '/add')
+    config.add_route('wikipage_view', '/{uid}')
+    config.add_route('wikipage_edit', '/{uid}/edit')
+    config.add_static_view('deform_static', 'deform:static/')
     config.scan('.views')
     return config.make_wsgi_app()
