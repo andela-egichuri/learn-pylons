aff3aa0084dbff2259a36f9ff055f3bd2124a3ac
Eric Gichuri <eric.gichuri@andela.com>
2016-06-21 18:28:41 +0300

    Logging app info

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,39 +1,23 @@
+import logging
+log = logging.getLogger(__name__)
+
 from pyramid.view import (
     view_config,
     view_defaults
     )
 
 
-@view_defaults(route_name='hello')
-class TutorialViews(object):
+@view_defaults(renderer='home.pt')
+class TutorialViews:
     def __init__(self, request):
         self.request = request
-        self.view_name = 'TutorialViews'
 
-    @property
-    def full_name(self):
-        first = self.request.matchdict['first']
-        last = self.request.matchdict['last']
-        return first + ' ' + last
-
-    @view_config(route_name='home', renderer='home.pt')
+    @view_config(route_name='home')
     def home(self):
-        return {'page_title': 'Home View'}
+        log.debug('In home view')
+        return {'name': 'Home View'}
 
-    # Retrieving /howdy/first/last the first time
-    @view_config(renderer='hello.pt')
+    @view_config(route_name='hello')
     def hello(self):
-        return {'page_title': 'Hello View'}
-
-    # Posting to /howdy/first/last via the "Edit" submit button
-    @view_config(request_method='POST', renderer='edit.pt')
-    def edit(self):
-        new_name = self.request.params['new_name']
-        return {'page_title': 'Edit View', 'new_name': new_name}
-
-    # Posting to /howdy/first/last via the "Delete" submit button
-    @view_config(request_method='POST', request_param='form.delete',
-                 renderer='delete.pt')
-    def delete(self):
-        print ('Deleted')
-        return {'page_title': 'Delete View'}
+        log.debug('In hello view')
+        return {'name': 'Hello View'}
